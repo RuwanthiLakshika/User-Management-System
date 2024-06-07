6ec75e9e87640e6d237eb1f627bad6069c76f8c2
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-06-07 07:08:30 +0530

    initial changes

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to User Management System</h1>
+        <button className='user-button'>Users</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
