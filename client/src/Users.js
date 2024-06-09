8ab045bff2f044107c5b5742ed72ed64426a2888
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-06-09 14:05:36 +0530

    get API intergration

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,26 +1,34 @@
 import UserForm from "./UserForm";
 import UsersTable from "./UsersTable";
 import { Box } from "@mui/material";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
-const users = [
-    {
-        id: 1,
-        name: 'Ruwanthi'
-    },
-    {
-        id: 2,
-        name: 'Nimal'
-    },
-];
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        Axios.get('http://localhost:3001/api/users')
+            .then(response => {
+               setUsers(response.data || []);
+        })
+        .catch(error => {
+            console.error("Error getting users:", error);
+        });
+    }
+
     return (
         <Box
-        sx={{
-            width: 'calc(100% - 100px)',
-            margin: 'auto',
-            marginTop: '100px',
-        }}
+            sx={{
+                width: 'calc(100% - 100px)',
+                margin: 'auto',
+                marginTop: '100px',
+            }}
         >
             <UserForm/>
             <UsersTable rows={users}/>
